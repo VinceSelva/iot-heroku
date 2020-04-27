1d0c731fb1b5d27e9f10d919cef61a9aecd553a0
VinceSelva <vincenzo.selva@hotmail.fr>
2020-04-27 12:08:06 +0200

    gitignore + server

--- projet-vue/server.js
+++ projet-vue/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname,'dist')));
-const port = 8080;
+app.use('/', serveStatic(path.join(__dirname,'dist')));
+
+const port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log('server started '+ port);
